d2e9a4feb3e75cd3805b6712499620281994522b
David I Samudio <luminaxster@gmail.com>
2024-03-13 13:48:42 -0400

    Update FordTest.java

--- CS211_P2/sublime/src/test/FordTest.java
+++ CS211_P2/sublime/src/test/FordTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.concurrent.TimeUnit;
 
 // Author: Dr. Ford
-// Guest, uncomment as you implemented the types.
+// Guest, uncomment as you implement the imported types below.
 import src.main.Bernard;
 import src.main.SystemWhole;
 // import src.main.Host;
@@ -15,30 +15,29 @@ import src.main.SystemWhole;
 // import src.main.MemorySnapshot;
 // import src.main.NarrativeLoop;
 
+// Author: Dr. Ford
 public class FordTest{
-    private final static String[] EMPTY_EMERGENCES ={};
-
     @Test
     @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
     @Tag("grading")
     public void testBernardSimulationDiagnostics000(){
-        // P2 emegences strings be like:
-        String[] emergences ={"kind:Human"};
-        // Using Bernard to get system wholes
-        SystemWhole aSystemWhole = Bernard.analysis(emergences);
-        // matching updateNarrativeLoops parameter types
-        SystemWhole[] systemWholes = {aSystemWhole};
-        // // Getting a narrative
-        // NarrativeLoop narrativeLoop = new MazeLoop();
-        // The part is in both, since it is the same array, it should have been added to the emulation list.
-        // narrativeLoop.updateNarrativeLoops(systemWholes,systemWholes);
-        // // Let's make sure of it, I can verify that via Host
-        // Host host = new Host(EMPTY_EMERGENCES, 0, false, true, true, narrativeLoop);
-        // // Tell me what you got host
-        // MemorySnapshot memorySnapshot = host.analyze();
-        // // Your simulated memories must have the aSystemWhole I created for you 
-        // assertTrue(memorySnapshot.getSimulationMemory().contains((Object)aSystemWhole));
-        // // This is a mockery of assertion. Disregard once the one above works.
-        assertTrue(true);
+             // P2 emergences strings be like:
+             String[] emergences = {"kind:Human"};
+             // Using Bernard to get systems (whole)
+             SystemWhole aSystemWhole = Bernard.analysis(emergences);
+             // matching updateNarrativeLoops parameter types
+             SystemWhole[] systemWholes = {aSystemWhole};
+             // Getting a narrative
+            //  NarrativeLoop narrativeLoop = new MazeLoop();
+            //  //The system is in both, since it is the same array, it should be added to the simulation list.
+            //  narrativeLoop.updateNarrativeLoops(systemWholes,systemWholes);
+            //  // Let's make sure of it, I can verify that via Host
+            //  Host host = new Host(narrativeLoop);
+            //  // Tell me what you got host
+            //  MemorySnapshot memorySnapshot = host.analyze();
+            //  // Your simulated memories must have the aSystemWhole I created for you 
+            //  assertTrue(memorySnapshot.getSimulationMemory().contains((Object)aSystemWhole));
+            //  // This is a mockery of an assertion. Disregard once the one above works.
+             assertTrue(true);
     }  
 }
